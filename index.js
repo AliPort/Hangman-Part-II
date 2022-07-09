b2b0e77f95e7761b549699ffd9f6c978632e2e73
AliPort <bwithtacos@gmail.com>
2022-07-09 10:28:38 -0500

    html almost complete

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
  't', 'u', 'v', 'w', 'x', 'y', 'z']]
 
+
+ //cited from gamedevelopment.tutsplus.com
+
+ //alphabet list
 const buttons = function () {
     myButtons = document.getElementById('buttons');
     letters = document.createElement('ul');
 
-    //cited from gamedevelopment.tutsplus.com
     for (var i = 0; i <alphabet.length; i++) {
         letters.id = 'alphabet';
         list = createElement('li');
@@ -18,4 +21,21 @@ const buttons = function () {
     }
 }
 
+//category
 
+const category = function () {
+    if ()
+
+}
+
+
+
+ 
+/*const lives;
+const hint;
+const clue; 
+const correctGuess;
+const wrongGuess;
+const guess;
+const selectedCategory;
+const word;*/
